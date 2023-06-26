9581e7cda8c15402b4dbb8eb19ef23340cfc67e1
qhqo0403 <119954728+qhqo0403@users.noreply.github.com>
2023-06-26 16:55:39 +0900

    mobile gnb

--- room/script/room.js
+++ room/script/room.js
@@ -43,3 +43,15 @@ const swiper = new Swiper('.swiper', {
   prevEl: '.swiper-button-prev',
   },
 });
+
+const toggleGnb = () => {
+  const toggleBtn = document.querySelector('#toggle');
+  const mobileGnb = document.querySelector('.m_gnb');
+
+  toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('on');
+    mobileGnb.classList.toggle('active');
+  })
+}
+
+toggleGnb();
--- script/gnb.js
+++ script/gnb.js
@@ -1,12 +1,3 @@
-const swiper = new Swiper(".swiper", {
-  autoplay: true,
-  speed: 1000,
-  scrollbar: {
-    el: ".swiper-scrollbar",
-    draggable: true
-  },
-});
-
 const toggleGnb = () => {
   const toggleBtn = document.querySelector('#toggle');
   const mobileGnb = document.querySelector('.m_gnb');
